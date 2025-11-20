f035b50d8c32966032dc94b4032f2d5dcf9791cb
Magi Aristondo Alzina <magi.aristondo@c6s302pc44.fib.upc.es>
2025-11-20 16:53:37 +0100

    P87920 MAL FET

---
+++ Parcial2/Vectors/com_la_suma_dels_demes.cc
@@ -0,0 +1,24 @@
+// // P87920
+
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+
+// void es_suma_demes(const int& n)
+// {
+//     int a = 0, suma = 0;
+
+//     for(int i = 0; i < n; ++i)
+//     {
+//         cin >> a;
+//         suma += a;
+//     }
+//     cout << n << " → " << suma;
+//     cout << ((suma == n) ? "YES" : "NO") << endl;
+// }
+
+// int main()
+// {
+//     int n;
+//     while(cin >> n) es_suma_demes(n);
+// } TOT MALAMENT MAL ENTÈS
